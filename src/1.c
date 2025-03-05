a31547fab9bb8b651540fe684d1e714623988d68
olmanpaguada134 <olmanpaguada134@gmail.com>
2025-03-05 22:30:36 +0000

    Add 1.c

---
+++ src/1.c
@@ -0,0 +1,13 @@
+int main(void) {
+    int num = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num > 0) {
+        printf("The number is positive.\n");
+    } else if (num < 0) {
+        printf("The number is negative.\n");
+    } else {
+        printf("The number is zero.\n");
+    }
+    return 0;
+}
