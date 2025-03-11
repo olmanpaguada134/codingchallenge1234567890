879c9c490c3ac1d5ebc5dc6cfe82dc2283621bdc
olmanpaguada134 <olmanpaguada134@gmail.com>
2025-03-11 15:50:58 +0000

    Add 7.c

---
+++ src/7.c
@@ -0,0 +1,11 @@
+  int main(void) {
+  int x = 5;
+  if (x > 0) {
+    printf("X is positive\n");
+  } else if (x < 0) {
+    printf("X is negative\n");
+  } else {
+    printf("X is zero\n");
+  }
+  return 0;
+}
