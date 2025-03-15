5ff966b284dfae26afab51bba09034f48a7e709d
olmanpaguada134 <olmanpaguada134@gmail.com>
2025-03-15 08:03:47 +0000

    Add 11.c

---
+++ src/11.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int x = 5;
+    double y = 3.14;
+    char c = 'a';
+
+    printf("x = %d, y = %f, c = %c\n", x, y, c);
+
+    return 0;
+}
