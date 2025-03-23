cd605ef996547ba0438b4dacfc5e19ea4492f50f
olmanpaguada134 <olmanpaguada134@gmail.com>
2025-03-23 04:34:11 +0000

    Add 19.c

---
+++ src/19.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main() {
+    int i;
+
+    for(i = 0; i < 5; i++) {
+        printf("*");
+    }
+
+    printf("\n");
+
+    for(i = 1; i <= 4; i++) {
+        printf("*");
+    }
+}
